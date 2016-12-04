70950cdc185b8b171f137fb79b95d5f6f31fe31a
bigbigsnail <hhy-yale@163.com>
2016-12-04 16:18:12 -0600

    Create branch Project 4 for CUDA program

---
+++ CUDA Program/CUDA Program/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CUDA Program
+//
+//  Created by bigbigsnail on 12/4/16.
+//  Copyright © 2016 bigbigsnail. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
